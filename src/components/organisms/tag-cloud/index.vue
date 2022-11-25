0f2ff7969be6725bcecfdf3c23c6606d8a55a8bb
lazlanrafar <lazlanrafar@gmail.com>
2022-11-25 13:38:20 +0700

    refactor about page

---
+++ src/components/organisms/tag-cloud/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="global" ref="skill"></div>
+</template>
+
+<script>
+import TagCloud from "TagCloud";
+
+export default {
+  name: "tag-cloud",
+  mounted() {
+    const container = this.$refs.skill;
+    var x = window.matchMedia("(max-width: 600px)");
+    let radius = 350;
+
+    if (x.matches) {
+      radius = 170;
+    }
+
+    const texts = [
+      "HTML 5",
+      "CSS 3",
+      "JavaScript",
+      "Vue.js",
+      "Next.js",
+      "Jquery",
+      "Bootstrap 5",
+      "Laravel",
+      "Tawilwind CSS",
+      "Wordpress",
+      "SASS",
+      "Git",
+      "TypeScript",
+      "Express.js",
+    ];
+    const options = {
+      radius: radius,
+      itemClass: "item",
+    };
+
+    TagCloud(container, texts, options);
+    document.querySelector(".tagcloud").style.margin = "auto";
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.global {
+  margin: 0 auto;
+  font-weight: 600;
+  color: var(--font-color);
+  font-family: "Poppins", sans-serif;
+  user-select: none;
+}
+
+@media only screen and (max-width: 1024px) {
+  .global {
+    display: block;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .global {
+    position: relative;
+    font-weight: 500;
+    font-size: 1rem;
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,73 +1,39 @@
 <template>
-  <div class="about">
-    <div class="container">
-      <div class="text">
-        <div class="code">h1</div>
-        <header-text
-          text="Me,Myself & I"
-          class="title"
-          open="true"
-          close="true"
-        ></header-text>
-        <div class="deks">
-          <i class="code">p</i>
-          <p style="margin-bottom: 1rem">
-            Hii I'm a Full Stack Web Developer based in Indonesia. I describe
-            myself as a passionate developer who loves coding, open source, and
-            the web platform.
-          </p>
-          <p>
-            Aside from my job, I like to create and contribute to open source
-            projects. That helps me to learn a ton of new stuff, grow as a
-            developer and support other open source projects. Also I enjoy
-            writing technical things at my blog.
-          </p>
-          <i class="code">/p</i>
-        </div>
+  <div class="content-body" id="about">
+    <div class="text">
+      <textCode text="h1" />
+      <textHeader
+        text="Me,Myself & I"
+        class="title"
+        open="true"
+        close="true"
+      ></textHeader>
+      <div class="deks">
+        <textCode text="p" />
+        <p style="margin-bottom: 1rem">
+          Hii I'm a Full Stack Web Developer based in Indonesia. I describe
+          myself as a passionate developer who loves coding, open source, and
+          the web platform.
+        </p>
+        <p>
+          Aside from my job, I like to create and contribute to open source
+          projects. That helps me to learn a ton of new stuff, grow as a
+          developer and support other open source projects.
+        </p>
+        <textCode text="p" isClose />
       </div>
-      <div class="global" ref="skill"></div>
     </div>
+    <tagCloud />
   </div>
 </template>
 
 <script>
-import TagCloud from "TagCloud";
-
 export default {
   name: "About",
   components: {
-    HeaderText: () => import("@/components/atoms/text-header.vue"),
-  },
-  mounted() {
-    const container = this.$refs.skill;
-    var x = window.matchMedia("(max-width: 600px)");
-    let radius = 350;
-
-    if (x.matches) {
-      radius = 200;
-    }
-
-    const texts = [
-      "HTML 5",
-      "CSS 3",
-      "JavaScript",
-      "Vue.js",
-      "Next.js",
-      "Jquery",
-      "Bootstrap 5",
-      "Laravel",
-      "Wordpress",
-      "SASS",
-      "Git",
-      "TypeScript",
-    ];
-    const options = {
-      radius: radius,
-      itemClass: "item",
-    };
-
-    TagCloud(container, texts, options);
-    document.querySelector(".tagcloud").style.margin = "auto";
+    textHeader: () => import("@/components/atoms/text-header.vue"),
+    textCode: () => import("@/components/atoms/text-code.vue"),
+    tagCloud: () => import("@/components/organisms/tag-cloud/index.vue"),
   },
 };
 </script>
